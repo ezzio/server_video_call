486c52642ec50e5ad46d593e7bca966a86a95da5
ezzio <thangduong1233@gmail.com>
2021-12-06 10:00:46 +0700

    them message disconnect

--- routes/RoomMeeting.js
+++ routes/RoomMeeting.js
@@ -7,29 +7,59 @@ router.post("/CreateMeetingRoom", async (req, res) => {
     RoomName: request.RoomName,
     owner: request.owner,
   });
-  await newMeeting.save((err, modal) => {
+  await newMeeting.save(async (err, modal) => {
     if (err) {
       res.send({ isSuccess: false });
     } else {
+      RoomMeeting.updateOne(
+        { _id: modal._id },
+        { $push: { Member: request.owner } }
+      );
       res.send({ isSuccess: true, roomId: modal.RoomName });
     }
   });
 });
-router.post("/CheckMeetingRoom", async (req, res) => {
+// router.post("/CheckMeetingRoom", async (req, res) => {
+//   let request = req.body;
+//   await RoomMeeting.find({ _id: request.RoomName }, (error) => {
+//     if (error) {
+//       res.send("Room is not exist");
+//     } else {
+//       res.send({ isSuccess: true });
+//     }
+//   });
+// });
+router.post("/ListAllMeetingRoom", async (req, res) => {
   let request = req.body;
-  await RoomMeeting.find({ _id: request.RoomName }, (error) => {
+  let allRoomMeeting = await RoomMeeting.find({ owner: request.owner })
+    .lean()
+    .exec();
+  if (allRoomMeeting.length > 0) {
+    res.send({allRoomMeeting, isSuccess: true});
+  } else {
+    res.send([{
+      
+    }]);
+  }
+});
+router.post("/deleteMeetingRoom", async (req, res) => {
+  let request = req.body;
+  await RoomMeeting.deleteOne({ _id: request.idRoon }, (error) => {
     if (error) {
-      res.send("Room is not exist");
+      res.send({ isSuccess: false });
     } else {
       res.send({ isSuccess: true });
     }
   });
 });
-
-router.post("/inviteToRoom", async (req, res) => {
+router.post("/CheckMeetingRoom", async (req, res) => {
   let request = req.body;
-  
-  
-})
+  let checkInRoom = await RoomMeeting.find({ RoomName: request.roomName });
+  if (checkInRoom.length > 0) {
+    res.send({ isSuccess: true });
+  } else {
+    res.send({ isSuccess: false });
+  }
+});
 
 module.exports = router;
--- routes/register.js
+++ routes/register.js
@@ -3,19 +3,29 @@ var router = express.Router();
 const functionAutho = require("../public/javascripts/CheckAutho");
 var User_Schema = require("../public/db/Schema/User_Schema");
 /* GET home page. */
-router.get("/",(req, res) => {
+router.get("/", (req, res) => {
   console.log("register");
 });
 
+
 router.post("/", async (req, res) => {
-  let User = new User_Schema(req.body);
-  await User.save(function (err, result) {
-    if (err) {
-      res.send({ redirect: "/register" });
-    } else {
-      console.log(result);
-    }
-  });
-  res.send({ isSuccess: true });
+  let InfoUser = req.body;
+  let checkUser = await user
+    .find({ username: InfoUser.username })
+    .lean()
+    .exec();
+  if (checkUser.length == 0) {
+    let User = new user({ ...req.body, gender: "male" });
+    await User.save(function (err, result) {
+      if (!err) {
+        res.send({ isSuccess: true });
+      } else {
+        res.send({ error: "Register fail", isSuccess: false });
+      }
+    });
+  } else {
+    res.send({ error: "Username is existed" });
+  }
 });
+
 module.exports = router;
